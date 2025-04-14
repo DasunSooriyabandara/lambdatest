d3bb7cdfaba7050fd07b4da92bf5244987a45d38
Dasun Sooriyabandara <115138972+DasunSooriyabandara@users.noreply.github.com>
2025-04-14 23:45:50 +0530

    homepage script

--- LT-ecommerce-Playwright-Java-main/src/main/java/com/lt/pages/HomePage.java
+++ LT-ecommerce-Playwright-Java-main/src/main/java/com/lt/pages/HomePage.java
@@ -26,6 +26,8 @@ public class HomePage {
 	private Locator comparetBtn;
 	private Locator comparetBtnInsidePage;
 	private Locator productCompareBtn;
+	private Locator carroselArrow;
+	private Locator carroselImage;
 
 	public HomePage(Page page) {
 		this.page = page;
@@ -38,11 +40,20 @@ public class HomePage {
 		this.selectOption = page.locator("(//a[@class='dropdown-item'])[3]");
 		this.searchField = page.locator("(//*[@name='search'])[1]");
 		this.searchBtn = page.locator("(//button[@type='submit'])[1]");
-
-		this.optionOne = page.locator("(//ul[@class='dropdown-menu autocomplete w-100'])[1]/li[1]");
 		this.comparetBtn = page.locator("//*[@id='entry_217823']");
+
+		
+		
+		this.optionOne = page.locator("//*[@id=\"search\"]/div[1]/div[2]/ul/li[1]/div[1]/a");
+		
 		this.comparetBtnInsidePage = page.locator("//*[@id='entry_216844']//child::button");
 		this.productCompareBtn = page.locator("#notification-box-top > div > div.toast-body > a");
+		
+		
+		this.carroselArrow = page.locator("(//span[@class='carousel-control-next-icon'])[1]");
+		this.carroselImage = page.locator("(//div[@class='carousel-item active'])[1]");
+		//this.carroselArrow = page.locator("(//div[starts-with(@class,'carousel-item')])[1]");
+				
 	}
 
 	public void searchFunction() {
@@ -59,18 +70,29 @@ public class HomePage {
 
 		String actualTitle = page.title();
 		System.out.println("Actual title: " + actualTitle);
-		Assert.assertEquals(actualTitle, "Your Store]");
+		Assert.assertEquals(actualTitle, "Search - HTC Touch HD");
 
+		page.waitForTimeout(3000);
 		page.goBack();
 
+		
+//		comparetBtn.click();
+//		Locator NoProd = page.locator("//p[text()='You have not chosen any products to compare.']");
+//		NoProd.textContent().trim();
+//		Assert.assertEquals(NoProd, "Locator@//p[text()='You have not chosen any products to compare.");
+//		Locator ContinueBtn = page.locator("//*[@id=\"content\"]/div/div/a");
+//		ContinueBtn.click();
+		
+		
 		searchCategoryBtn.click();
-		page.waitForTimeout(3000);
 		selectOption.click();
 
 		searchField.type("HTC");
 		optionOne.click();
+		System.out.println("clikc option one");
 
 		String actualTitlesearch = page.title();
+		//actualTitlesearch.wait(new Locator.WaitForOptions().setState(WaitForSelectorState.VISIBLE));
 		Assert.assertEquals(actualTitlesearch, "HTC Touch HD");
 
 	}
@@ -93,9 +115,13 @@ public class HomePage {
 		System.out.println("Actual title: " + actualTitleProdCompare);
 		Assert.assertEquals(actualTitleProdCompare, "Product Comparison");
 
-		Locator SuccessNotificstions = page.locator("#product-compare > div.alert.alert-success.alert-dismissible");
 		
-
+		Locator RemoveBtn = page.locator("//*[@id=\"content\"]/table/tbody[2]/tr/td[2]/a");
+		RemoveBtn.click();
+		page.waitForTimeout(3000);
+		
+		
+		Locator SuccessNotificstions = page.locator("#product-compare > div.alert.alert-success.alert-dismissible");
 		// Wait for the notification to appear (optional if it takes time)
 		SuccessNotificstions.waitFor(new Locator.WaitForOptions().setState(WaitForSelectorState.VISIBLE));
 
@@ -104,6 +130,34 @@ public class HomePage {
 		Assert.assertTrue(SuccessNotificstions.isVisible(), "Notification did not appear!");
 
 		System.out.println("Successfully disply notification Compair this product");
+		
+		
+		Locator ContinueBtn = page.locator("//*[@id=\"content\"]/div/div/a");
+		ContinueBtn.click();
+		System.out.println("SUccesssfully click the continue buttona and come to Home page");
+		
 
+	}
+	
+	public void BannerOptions() {
+		
+		carroselArrow.click();
+		
+		
+		// Extract the URL from the button using JavaScript
+		String targetUrl = (String) page.evaluate("() => document.querySelector('button#yourButtonId').getAttribute('onclick')");
+		// OR if the button triggers a link behind the scenes, get that URL instead
+
+		// Open a new page (tab)
+		Page newTab = page.context().newPage();
+		newTab.navigate(targetUrl);
+
+		// Now interact with the new tab
+		System.out.println("Opened URL in new tab: " + newTab.url());
+
+		
+		carroselImage.click();
+		
+		
 	}
 }
